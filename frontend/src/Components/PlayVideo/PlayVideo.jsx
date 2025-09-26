51889464caeb435cf3449a6bf6085b53a926eabd
Chido <chido@example.com>
2025-09-26 13:17:41 -0700

    Move buttons much closer to video player with negative margin

--- frontend/src/Components/PlayVideo/PlayVideo.jsx
+++ frontend/src/Components/PlayVideo/PlayVideo.jsx
@@ -1262,7 +1262,7 @@ const PlayVideo = ({ videoId: propVideoId, thumbnail, setThumbnail, screenshots,
                  display: 'flex',
                  gap: '10px',
                  marginBottom: isMobile ? '0px' : '0px',
-                 marginTop: isMobile ? '2px' : '2px',
+                 marginTop: isMobile ? '-10px' : '-10px',
                  flexWrap: 'wrap'
              }}>
                 <button 
