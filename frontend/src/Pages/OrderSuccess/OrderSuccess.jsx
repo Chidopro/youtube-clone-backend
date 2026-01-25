0dbdd17a85a773e7dfe7393f047c3b1757d9cf69
Alan Armstrong <chidopro@proton.me>
2026-01-25 10:21:29 -0800

    Update OrderSuccess to match second image design - Remove boxes around list items, use simple checkmarks - Make layout landscape (wider, less tall) - Reduce spacing to fit on screen without scrolling - Add 'What happens next:' heading - Match exact text from second image

--- frontend/src/Pages/OrderSuccess/OrderSuccess.jsx
+++ frontend/src/Pages/OrderSuccess/OrderSuccess.jsx
@@ -95,6 +95,7 @@ const OrderSuccess = () => {
         </div>
         
         <div className="next-steps">
+          <h3>What happens next:</h3>
           <ul>
             <li>Your order is being processed automatically</li>
             <li>Products will be created and fulfilled by Printful</li>
